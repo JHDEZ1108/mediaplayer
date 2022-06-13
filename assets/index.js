d7c05dc5c807e80e9bbe84b70e7a57af7f29ae02
Josué Hernández <joheca1998@outlook.com>
2022-06-13 10:34:24 -0600

    Integrando plugin de Autoplay e integrando botón de mute

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const buttonPlay = document.getElementById('play-pause')
+buttonPlay.onclick = () => player.togglePlay();
+
+const buttonMute = document.getElementById('mute-unmute');
+buttonMute.onclick = () => player.toggleMute();
