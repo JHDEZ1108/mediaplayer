cbc634e7931bdeec3e5019d76c45ec89789a2560
Josué Hernández <joheca1998@outlook.com>
2022-06-28 09:29:02 -0600

    Integrando IntersectionObserver para pausar automaticamente el video

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -6,8 +6,26 @@ function MediaPlayer(config) {
 }
 
 MediaPlayer.prototype._initPlugins = function(){
+    const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media,
+        
+        get muted(){
+            return this.media.muted;
+        },
+        set muted(value){
+/*             if(value === true){
+                this.media.muted = true
+            }else{
+                this.media.muted = false
+            } */
+            this.media.muted = value
+        }
+    };
+    
     this.plugins.forEach(plugin => {
-        plugin.run(this);
+        plugin.run(player);
     })
 }
 
